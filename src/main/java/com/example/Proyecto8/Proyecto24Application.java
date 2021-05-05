19fb889295164850770fde6de34c90695a462084
jehuamanr@uni.pe <jehuamanr@uni.pe>
2021-05-05 10:45:48 -0500

    asdasd

--- src/main/java/com/example/Proyecto8/Proyecto24Application.java
+++ src/main/java/com/example/Proyecto8/Proyecto24Application.java
@@ -4,20 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.HashMap;
-import java.util.TreeMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 @SpringBootApplication
 @EnableJpaAuditing
-public class Proyecto8Application {
+public class Proyecto24Application {
 
 	public static void main(String[] args) {
 		//Map<Integer,String> l=new LinkedHashMap<Integer,String>();
 		//Map<Integer,String> m=new HashMap<Integer,String>();
 		//Map<Integer,String> n=new TreeMap<Integer,String>();
-		SpringApplication.run(Proyecto8Application.class, args);
+		SpringApplication.run(Proyecto24Application.class, args);
 	}
 
 }
